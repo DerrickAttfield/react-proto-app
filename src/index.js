e05863834f3fa680c4ec368b969108235be52933
Derrick Attfield <DerrickAttfield@potentiosoft.com>
2023-05-23 02:03:21 +0200

    react-proto-app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!!');
+// var element2 = React.createElement('h2', { className: 'greeting' }, 'This is a lot more organized!!');
+
+// ReactDOM.render(element, document.getElementById('root'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  element
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
