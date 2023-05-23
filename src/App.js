64d918896c06003c4784b15daa676ac13a4b2158
Derrick Attfield <DerrickAttfield@potentiosoft.com>
2023-05-23 02:54:55 +0200

    purpose restated

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
           Here we are Galaxy!!.
         </p>
         <p>
-          We build software to digitally empower EVERYONE.
+        We invent and create digital technologies to empower everyone.
         </p>
       </header>
     </div>
