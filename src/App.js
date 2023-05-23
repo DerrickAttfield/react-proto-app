640e82006e912d6157c2cb9754588c02056cf3e1
Derrick Attfield <DerrickAttfield@potentiosoft.com>
2023-05-23 02:40:01 +0200

    edit to demonstrate can be done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import logo from './logo.svg';
+/* eslint-disable react/react-in-jsx-scope */
+import logo from './PotentioSoft Logo _v5.png';
 import './App.css';
 
 function App() {
@@ -7,16 +8,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Here we are Galaxy!!.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          We build software for the to digitally enable EVERYONE.
+        </p>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!!');
+// var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!!');
 // var element2 = React.createElement('h2', { className: 'greeting' }, 'This is a lot more organized!!');
 
 // ReactDOM.render(element, document.getElementById('root'));
 
+// eslint-disable-next-line no-undef
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  element
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+  // element
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
